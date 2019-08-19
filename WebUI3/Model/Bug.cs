d99dfdf508a7ff2b6fbf15afc7396c47751417d2
Viacheslav Emtcov <viacheslav.emtkov@datium.fi>
2019-08-19 18:16:31 +0300

    styles + started adding db

---
+++ WebUI3/Model/Bug.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebUI3.Model
+{
+    public class Bug
+    {
+        [Key]
+        public int BugId { get; set; }
+        public string Description { get; set; }
+        public int TypeId { get; set; }
+        public int StatusId { get; set; }
+        public int AreaId { get; set; }
+        public int UserId { get; set; }
+        public DateTime DateCreation { get; set; }
+    }
+}
